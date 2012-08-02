a8b6c2fdf8f5caa79ee8444068e1048a389c5003
Philipp Geyer <philipp@geyer.co.uk>
2012-08-02 19:13:01 +0100

    first attempt at scriptable behaviour

---
+++ source/Util/ScriptBehaviour.cpp
@@ -0,0 +1,94 @@
+/* ScriptBehaviour.cpp
+  version 0.0.2, August 2st, 2012
+
+  Copyright (C) 2012 Philipp Geyer
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Philipp Geyer
+*/
+
+/* Changelog
+   0.0.2 - 01.08.2012 - Renamed to ScriptBehaviour. Added state machine
+                        skeleton to handle controlling the player. - PG
+   0.0.1 - 12.02.2012 - Implemented as TuturialBehaviour for 
+                        http://nistur.com - PG
+*/
+
+#include "ScriptBehaviour.h"
+
+#include <MEngine.h>
+
+REGISTER_BEHAVIOUR(ScriptBehaviour);
+//----------------------------------------
+// Messages
+//----------------------------------------
+
+//----------------------------------------
+// quick defines
+//----------------------------------------
+
+//----------------------------------------
+// ScriptBehaviour
+//----------------------------------------
+ScriptBehaviour::ScriptBehaviour(MObject3d * parentObject)
+: Behaviour(parentObject, GetStaticID())
+, m_FunctionName("")
+{
+}
+//----------------------------------------
+ScriptBehaviour::ScriptBehaviour(ScriptBehaviour & behavior, 
+							MObject3d * parentObject)
+: Behaviour(parentObject, GetStaticID())
+, m_FunctionName(behavior.m_FunctionName)
+{
+}
+//----------------------------------------
+ScriptBehaviour::~ScriptBehaviour(void)
+{
+}
+//----------------------------------------
+void ScriptBehaviour::destroy()
+{
+	delete this;
+}
+//----------------------------------------
+MBehavior* ScriptBehaviour::getNew(MObject3d* parentObject)
+{
+	return new ScriptBehaviour(parentObject);
+}
+//----------------------------------------
+MBehavior* ScriptBehaviour::getCopy(MObject3d* parentObject)
+{
+	return new ScriptBehaviour(*this, parentObject);
+}
+//----------------------------------------
+void ScriptBehaviour::update()
+{
+    MScriptContext* script = MEngine::getInstance()->getScriptContext();
+    MScene* scene = MEngine::getInstance()->getLevel()->getCurrentScene();
+
+    unsigned int id;
+    scene->getObjectIndex(getParentObject()->getName(), &id);
+
+    script->pushInteger(id);
+    script->callFunction(m_FunctionName.getData());
+}
+//----------------------------------------
+void ScriptBehaviour::OnMessage(Message message, int param1)
+{
+}
---
+++ source/Util/ScriptBehaviour.h
@@ -0,0 +1,73 @@
+/* ScriptBehaviour.h
+  version 0.0.2, August 2nd, 2012
+
+  Copyright (C) 2012 Philipp Geyer
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Philipp Geyer
+*/
+
+/* Changelog
+   0.0.2 - 01.08.2012 - Renamed to ScriptBehaviour. Added state machine
+                        skeleton to handle controlling the player. - PG
+   0.0.1 - 12.02.2012 - Implemented as TuturialBehaviour for 
+                        http://nistur.com - PG
+*/
+
+
+#ifndef __PLAYER_CONTROLLER_H__
+#define __PLAYER_CONTROLLER_H__
+
+#include "System/Timer.h"
+
+#include "System/MessageSystem.h"
+
+#include "System/BehaviourDB.h"
+
+//--------------------------------------------
+// ScriptBehaviour
+//--------------------------------------------
+class ScriptBehaviour : public Behaviour, public Observer
+{
+public:
+	ScriptBehaviour(MObject3d * parentObject);
+	ScriptBehaviour(ScriptBehaviour & behavior, MObject3d * parentObject);
+	~ScriptBehaviour();
+	
+	//----------------------------------------
+	// MBehavior virtuals
+	//----------------------------------------
+	void destroy();
+	static MBehavior* getNew(MObject3d* parentObject);
+	MBehavior* getCopy(MObject3d* parentObject);
+
+	void update();
+	void runEvent(int param){}
+
+	//----------------------------------------
+	// Observer virtuals
+	//----------------------------------------
+	void OnMessage(Message message, int param1);
+
+	IMPLEMENT_BEHAVIOUR(ScriptBehaviour);
+private:
+
+	MString m_FunctionName;
+};
+
+#endif /*__PLAYER_CONTROLLER_H__*/
