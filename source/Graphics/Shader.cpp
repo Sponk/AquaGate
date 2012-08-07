a8fc565985cdb88adf3484b1202d6ee14f5bff1b
Philipp Geyer <philipp@geyer.co.uk>
2012-08-07 07:53:06 +0100

    Tidying up shader and making it work post-publish

--- source/Graphics/Shader.cpp
+++ source/Graphics/Shader.cpp
@@ -3,80 +3,39 @@
 #include <MEngine.h>
 
 Shader::Shader(const char* vertexName, const char* fragmentName)
-: m_FX(0)
-, m_VertShad(0)
-, m_PixShad(0)
+: m_fx(0)
+, m_vertShad(0)
+, m_pixShad(0)
 {
-	MEngine* engine = MEngine::getInstance();
-	MSystemContext* system = engine->getSystemContext();
-
-	char pVertexName[0xff];
-	getGlobalFilename(pVertexName, system->getWorkingDirectory(), vertexName);
-	char pFragmentName[0xff];
-	getGlobalFilename(pFragmentName, system->getWorkingDirectory(), fragmentName);
-
-	// Load the vertex shader
-	MFile* vertFP = M_fopen(pVertexName, "rb+");
-	M_fseek(vertFP, 0L, SEEK_END);
-	long vsize = M_ftell(vertFP);
-	M_fseek(vertFP, 0L, SEEK_SET);
-
-	char* vertShader = new char[vsize+1];
-	M_fread(vertShader, 1, vsize, vertFP);
-	M_fclose(vertFP);
-	vertShader[vsize] = 0;
-
-	// Load the fragment shader
-	MFile* fragFP = M_fopen(pFragmentName, "rb+");
-	M_fseek(fragFP, 0L, SEEK_END);
-	long fsize = M_ftell(fragFP);
-	M_fseek(fragFP, 0L, SEEK_SET);
-
-	char* fragShader = new char[fsize+1];
-	M_fread(fragShader, 1, fsize, fragFP);
-	M_fclose(fragFP);
-	fragShader[fsize] = 0;
-
-	if(MRenderingContext* render = engine->getRenderingContext())
-	{
-		render->createVertexShader(&m_VertShad);
-		render->sendShaderSource(m_VertShad, vertShader);
-
-		render->createPixelShader(&m_PixShad);
-		render->sendShaderSource(m_PixShad, fragShader);
-
-		render->createFX(&m_FX, m_VertShad, m_PixShad);
-	}
-
-	delete[] vertShader;
-	delete[] fragShader;
+    sprintf(m_filenames[0], vertexName);
+    sprintf(m_filenames[1], fragmentName);
 }
 
 void Shader::Apply()
 {
-	if(m_FX == 0)
-		return;
-
-	MEngine* engine = MEngine::getInstance();
-	if(MRenderingContext* render = engine->getRenderingContext())
-		render->bindFX(m_FX);
+    if(m_fx == 0 && !Init())
+	return;
+    
+    MEngine* engine = MEngine::getInstance();
+    if(MRenderingContext* render = engine->getRenderingContext())
+	render->bindFX(m_fx);
 }
 
 void Shader::Clear()
 {
-	if(m_FX == 0)
-		return;
-
-	MEngine* engine = MEngine::getInstance();
-	if(MRenderingContext* render = engine->getRenderingContext())
-		render->bindFX(0);
+    if(m_fx == 0)
+	return;
+    
+    MEngine* engine = MEngine::getInstance();
+    if(MRenderingContext* render = engine->getRenderingContext())
+	render->bindFX(0);
 }
 
 void Shader::SetValue(const char* name, int val) const
 {
     MEngine* engine = MEngine::getInstance();
     if(MRenderingContext* render = engine->getRenderingContext())
-	render->sendUniformInt(m_FX , name, &val, 1);
+	render->sendUniformInt(m_fx , name, &val, 1);
 }
 
 
@@ -84,5 +43,54 @@ void Shader::SetValue(const char* name, float val) const
 {
     MEngine* engine = MEngine::getInstance();
     if(MRenderingContext* render = engine->getRenderingContext())
-	render->sendUniformFloat(m_FX , name, &val, 1);
+	render->sendUniformFloat(m_fx , name, &val, 1);
+}
+
+bool Shader::Init()
+{
+    MEngine* engine = MEngine::getInstance();
+    MSystemContext* system = engine->getSystemContext();
+    
+    char pVertexName[0xff];
+    getGlobalFilename(pVertexName, system->getWorkingDirectory(), m_filenames[0]);
+    char pFragmentName[0xff];
+    getGlobalFilename(pFragmentName, system->getWorkingDirectory(), m_filenames[1]);
+    
+    // Load the vertex shader
+    MFile* vertFP = M_fopen(pVertexName, "rb+");
+    M_fseek(vertFP, 0L, SEEK_END);
+    long vsize = M_ftell(vertFP);
+    M_fseek(vertFP, 0L, SEEK_SET);
+    
+    char* vertShader = new char[vsize+1];
+    M_fread(vertShader, 1, vsize, vertFP);
+    M_fclose(vertFP);
+    vertShader[vsize] = 0;
+    
+    // Load the fragment shader
+    MFile* fragFP = M_fopen(pFragmentName, "rb+");
+    M_fseek(fragFP, 0L, SEEK_END);
+    long fsize = M_ftell(fragFP);
+    M_fseek(fragFP, 0L, SEEK_SET);
+    
+    char* fragShader = new char[fsize+1];
+    M_fread(fragShader, 1, fsize, fragFP);
+    M_fclose(fragFP);
+    fragShader[fsize] = 0;
+    
+    if(MRenderingContext* render = engine->getRenderingContext())
+    {
+	render->createVertexShader(&m_vertShad);
+	render->sendShaderSource(m_vertShad, vertShader);
+	
+	render->createPixelShader(&m_pixShad);
+	render->sendShaderSource(m_pixShad, fragShader);
+	
+	render->createFX(&m_fx, m_vertShad, m_pixShad);
+    }
+    
+    delete[] vertShader;
+    delete[] fragShader;
+
+    return m_fx != 0;
 }
--- source/Graphics/Shader.h
+++ source/Graphics/Shader.h
@@ -4,20 +4,23 @@
 class Shader
 {
 public:
-	Shader(const char* vertexName, const char* fragmentName);
-
-	void Apply();
-	void Clear();
-
-	unsigned int ExposeShader() { return m_FX; }
-
-	void SetValue(const char* name, int val) const;
-	void SetValue(const char* name, float val) const;
+    Shader(const char* vertexName, const char* fragmentName);
+    
+    void Apply();
+    void Clear();
+    
+    unsigned int ExposeShader() { return m_fx; }
+    
+    void SetValue(const char* name, int val) const;
+    void SetValue(const char* name, float val) const;
 private:
-	
-	unsigned int m_FX;
-	unsigned int m_VertShad;
-	unsigned int m_PixShad;
+    bool Init();
+    
+    unsigned int m_fx;
+    unsigned int m_vertShad;
+    unsigned int m_pixShad;
+    
+    char         m_filenames[2][256];
 };
 
 #endif /*__SHADER_H__*/
